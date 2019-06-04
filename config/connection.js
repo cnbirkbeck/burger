1954889fc248af4a2683c4b0726dfbde60b0cf6a
CBirkbeck <cnbirkbeck@gmail.com>
2019-06-03 18:07:44 -0700

    modified connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ else {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'Cb11988!', // Add your password
+        password: '', // Add your password
         database: 'burgers_db' // Add your database
     });
 }
